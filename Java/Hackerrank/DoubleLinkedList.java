4f096470d92f45e79ef843244fe58aea8f9714fe
Michael Hummel <Mahummel1200@gmail.com>
2022-09-16 15:12:00 -0400

    linked list completion

--- Java/Hackerrank/DoubleLinkedList.java
+++ Java/Hackerrank/DoubleLinkedList.java
@@ -54,4 +54,56 @@ public class DoubleLinkedList {
       }
     }
   }
+
+  /**
+   * Insert data at proper location between two points of doubly linked list.
+   * SOLUTION TAKEN FROM DISCUSSION FORUMS No credit.
+   * 
+   * @param llist - linked list provided
+   * @param data  - data to create new node with
+   * @return - head of llist
+   */
+  public static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode llist, int data) {
+    DoublyLinkedListNode curr = llist;
+    DoublyLinkedListNode newNode = new DoublyLinkedListNode(data);
+
+    if (curr.data > data) {
+      newNode.next = curr;
+      return newNode;
+    }
+
+    while (curr.next != null && curr.data < data) {
+      curr = curr.next;
+    }
+
+    if (curr.next == null && data > curr.data) {
+      curr.next = newNode;
+      newNode.prev = curr;
+    } else {
+      newNode.next = curr;
+      newNode.prev = curr.prev;
+      curr.prev.next = newNode;
+      curr.prev = newNode;
+    }
+
+    return llist;
+  }
+
+  /**
+   * Reverse the doubly linked list
+   * 
+   * @param llist - list to reverse
+   * @return - head of newly reversed linked list
+   */
+  public static DoublyLinkedListNode reverse(DoublyLinkedListNode llist) {
+    DoublyLinkedListNode curr = llist;
+    DoublyLinkedListNode previous = null;
+    while (curr != null) {
+      DoublyLinkedListNode next = curr.next;
+      curr.next = previous;
+      previous = curr;
+      curr = next;
+    }
+    return previous;
+  }
 }
