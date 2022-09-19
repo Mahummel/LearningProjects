5c045a33a1cb1418af0ab81730381cd27d5060b9
Michael Hummel <Mahummel1200@gmail.com>
2022-09-19 19:51:54 -0400

    Tree in java as well

--- Java/Hackerrank/Tree.java
+++ Java/Hackerrank/Tree.java
@@ -1,8 +1,76 @@
 package Hackerrank;
 
-public class Tree {
-  
+public class Tree<V> {
+  class Node<V> {
+    V value;
+    Node<V> left = null;
+    Node<V> right = null;
+    Node(V value) {
+      this.value = value;
+    }
+  }
+
+  Node<V> base = null;
+  public void insert(V value) {
+    if (this.base == null) base = new Node<V>(value);
+    else {
+      Node<V> current = base;
+      while (true) {
+        if (value instanceof String && current.value instanceof String) {
+          String val = (String) value;
+          String currVal = (String) current.value;
+          if (val.compareTo(currVal) > 0) {
+            if (current.right != null) current = current.right;
+            else {
+              current.right = new Node<V>(value);
+              break;
+            }
+          } else if (val.compareTo(currVal) < 0) {
+            if (current.left != null) current = current.left;
+            else {
+              current.left = new Node<V>(value);
+              break;
+            }
+          } else {
+            break;
+          }
+        } else if (value instanceof Number && current.value instanceof Number) {
+          double val = ((Number) value).doubleValue();
+          double currVal = ((Number) current.value).doubleValue();
+          if (val > currVal) {
+            if (current.right != null) current = current.right;
+            else {
+              current.right = new Node<V>(value);
+              break;
+            }
+          } else if (val < currVal) {
+            if (current.left != null) current = current.left;
+            else {
+              current.left = new Node<V>(value);
+              break;
+            }
+          } else {
+            break;
+          }
+        } else {
+          break;
+        }
+      }
+    }
+  }
+  public void inOrder(Node<V> base) {
+    if (base == null) return;
+    inOrder(base.left);
+    System.out.print(base.value + " ");
+    inOrder(base.right);
+  }
   public static void main(String[] args) {
-    
+    Tree<String> test = new Tree<>();
+    test.insert("a");
+    test.insert("c");
+    test.insert("b");
+    test.insert("e");
+    test.insert("d");
+    test.inOrder(test.base);
   }
 }
--- Python/DataStructures/Tree.py
+++ Python/DataStructures/Tree.py
@@ -1,5 +1,5 @@
 class TreeNode:
-  def __init__(self, value):
+  def __init__(self, value) -> None:
     self.info = value
     self.left = None
     self.right = None
@@ -9,19 +9,44 @@ class TreeNode:
     return str(self.info)
 
   @staticmethod
-  def preOrder(root):
-    print(root)
-    if(root.left):
+  def preOrder(root) -> None:
+    print(root, end = " ")
+    if root.left:
       TreeNode.preOrder(root.left)
-    if(root.right):
+    if root.right:
       TreeNode.preOrder(root.right)
+
+  @staticmethod
+  def postOrder(root) -> None:
+    if root.left:
+      TreeNode.postOrder(root.left)
+    if root.right:
+      TreeNode.postOrder(root.right)
+    print(root, end = " ")
+
+  @staticmethod
+  def inOrder(root) -> None:
+    def dfs(root) -> list:
+      if root == None:
+        return []
+      return dfs(root.left) + [root.info] + dfs(root.right)
+    print(*dfs(root))
+  
+  @staticmethod
+  def height(root) -> int:
+    def dfs(root, counter) -> int:
+      if root == None:
+        return 1
+      return dfs(root.left) + dfs(root.right)
+    return dfs(root, -1)
+
 
 
 class BinarySearchTree:
-  def __init__ (self):
+  def __init__ (self) -> None:
     self.root = None
   
-  def create(self, val):
+  def create(self, val) -> None:
     if self.root == None:
       self.root = TreeNode(val)
     else:
@@ -43,13 +68,6 @@ class BinarySearchTree:
             break
         
 if __name__ == '__main__':
-  tree = BinarySearchTree()
-  tree.create(1)
-  tree.create(2)
-  tree.create(5)
-  tree.create(3)
-  tree.create(6)
-  tree.create(4)
-  TreeNode.preOrder(tree.root)
+  pass
 
     
